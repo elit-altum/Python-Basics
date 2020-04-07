4fc467869b427d7fb8c2de015a067001cc5cef4a
elit-altum <manan.sharma311@gmail.com>
2020-04-08 01:53:06 +0530

    fundamental data types

---
+++ dataTypes.py
@@ -0,0 +1,7 @@
+# practice data types and type casting
+
+print( 9.9 - 9 ) # returns 0.9000000000000004 due to floating approximation
+
+print(type(9.9 + 1)) # returns float
+
+print( 4 / 5 ) # returns 0.8 auto type cast
