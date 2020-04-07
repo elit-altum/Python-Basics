ffa14abf33677745cbb5d7b18ea1688bf01356e1
elit-altum <manan.sharma311@gmail.com>
2020-04-08 02:18:47 +0530

    Math functions

--- dataTypes.py
+++ dataTypes.py
@@ -5,3 +5,7 @@ print( 9.9 - 9 ) # returns 0.9000000000000004 due to floating approximation
 print(type(9.9 + 1)) # returns float
 
 print( 4 / 5 ) # returns 0.8 auto type cast
+
+# practice math functions
+print( round(3.5) )
+print( abs(-25) )
