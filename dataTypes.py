7931a3daddba57f4618e3d3a863e8d9b57b17c7a
elit-altum <manan.sharma311@gmail.com>
2020-04-08 23:24:05 +0530

    strings

--- dataTypes.py
+++ dataTypes.py
@@ -1,11 +1,20 @@
 # practice data types and type casting
 
-print( 9.9 - 9 ) # returns 0.9000000000000004 due to floating approximation
+# returns 0.9000000000000004 due to floating approximation
+print(9.9 - 9) 
 
 print(type(9.9 + 1)) # returns float
 
-print( 4 / 5 ) # returns 0.8 auto type cast
+print(4 / 5) # returns 0.8 auto type cast
 
 # practice math functions
-print( round(3.5) )
-print( abs(-25) )
+print(round(3.5))
+print(abs(-25))
+print(bin(1023))
+
+# complex
+# uses 'j' instead of 'i' for complex plane 
+print(type(2 + 3j))
+
+# data type actions
+print(int(5.9))
---
+++ strings.py
@@ -0,0 +1,11 @@
+# works with strings!
+
+name = '0123456'
+
+# string slicing
+# follows the syntax <string>[start_index:end_index:step_over]
+# of the form [start_index, end_index)
+print(name[3:7:2])
+
+# string reversal
+print(name[::-1])
