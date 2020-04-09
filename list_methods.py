858fe81557c5f9695ad29a2d73394f4e4a281874
elit-altum <manan.sharma311@gmail.com>
2020-04-09 22:10:44 +0530

    lists basics

---
+++ list_methods.py
@@ -0,0 +1,18 @@
+# list methods
+items = [1,2,3,4]
+
+# adds to the end of list
+items.append(100)
+print(items)
+
+# adds at a particular index
+items.insert(1, 44)
+print(items)
+
+# removes last item
+items.pop()
+print(items)
+
+# removes an item by value
+items.remove(44)
+print(items)
