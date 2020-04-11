f203470a35832d0f3d8e06c6c82e992250b14842
elit-altum <manan.sharma311@gmail.com>
2020-04-11 19:03:16 +0530

    conditonals

---
+++ conditionals.py
@@ -0,0 +1,17 @@
+# works with elif block conditionals
+is_old = False
+is_license = False
+
+age = int(input('Enter your age: '))
+is_old = age >= 18
+
+# 0 as a string evaluates to true so have to convert it to int()
+is_license = bool(int(input('Do you have a license 0/1: ')))
+
+# elif block
+if is_old and is_license:
+  print('You are good')
+elif is_old or is_license:
+  print('Please complete the left-over checks')
+else:
+  print('You can\'t drive')
